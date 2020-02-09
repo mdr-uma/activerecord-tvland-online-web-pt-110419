97b0035eb716ee9710f720f0adec37721163500b
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-09 17:21:57 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -1,5 +1,6 @@
 class AddColumnsToShows < ActiveRecord::Migration[5.1]
   def change
-    add_column :shows, :day, :genre, :season, :string
+    add_column :shows, :day, :string
+    add_column :genre, :season, :string
   end
 end
