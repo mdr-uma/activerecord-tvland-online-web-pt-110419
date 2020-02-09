ace065a74beac46449ed2a53398bd5ad9321a64e
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-09 16:26:19 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_character_to_the_actors.rb
+++ db/migrate/006_add_character_to_the_actors.rb
@@ -1,4 +1,4 @@
-class AddCharaterToTheActor < ActiveRecord::Migration[5.1]
+class AddCharaterToTheActors < ActiveRecord::Migration[5.1]
 
   def change
     add_column :characters, :catchphrase, :string
